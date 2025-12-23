5a4ff697e0d2628559684ef3601016313677cace
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-12-23 11:41:54 +0100

    Extract transitions from parser

--- src/main/scala/smc/syntaxAnalyzer/SyntacticalAnalyzer.scala
+++ src/main/scala/smc/syntaxAnalyzer/SyntacticalAnalyzer.scala
@@ -51,56 +51,12 @@ class SyntacticalAnalyzer(builder: SyntaxBuilder) extends TokenCollector {
   override def error(line: Int, position: Int): Unit =
     builder.syntaxError(line, position)
 
-  private val transitions: List[Transition] = List(
-    Transition(MachineDeclaration, Machine, MachineValue, None),
-    Transition(MachineValue, Name, MachineNamed, Some(_.addMachine())),
-    Transition(MachineNamed, OpenBrace, MachineSpec, None),
-    Transition(MachineNamed, Arrow, InitialArrow, None),
-    Transition(InitialArrow, Name, InitialArrowNamed, Some(_.setInitialState())),
-    Transition(InitialArrowNamed, OpenBrace, MachineSpec, None),
-    Transition(MachineSpec, Initial, InitialValue, None),
-    Transition(InitialValue, Name, MachineSpec, Some(_.setInitialState())),
-    Transition(MachineSpec, ClosedBrace, MachineDeclaration, Some(_.concludeStateMachine())),
-    Transition(MachineDeclaration, End, MachineDeclaration, None),
-    Transition(MachineSpec, State, StateValue, None),
-    Transition(StateValue, OpenBrace, SubtransitionSpec, Some(_.markAsSubtransition())),
-    Transition(StateValue, Name, EventArrow, Some(_.addTransition())),
-    Transition(EventArrow, Arrow, EventValue, None),
-    Transition(EventValue, Name, NextStateArrow, Some(_.setEvent())),
-    Transition(NextStateArrow, Arrow, NextStateValue, None),
-    Transition(NextStateValue, Dash, ActionArrow, Some(_.setEmptyNextState())),
-    Transition(NextStateValue, Name, ActionArrow, Some(_.setNextState())),
-    Transition(ActionArrow, ClosedBrace, MachineDeclaration, Some(_.concludeTransition())),
-    Transition(ActionArrow, Initial, InitialValue, Some(_.concludeTransition())),
-    Transition(ActionArrow, Superstate, SuperstateValue, Some(_.concludeTransition())),
-    Transition(ActionArrow, Arrow, ActionDeclaration, None),
-    Transition(ActionDeclaration, Name, MachineSpec, Some(_.setActionAndConclude())),
-    Transition(ActionDeclaration, OpenBrace, ActionValue, None),
-    Transition(ActionValue, Name, ActionValue, Some(_.addAction())),
-    Transition(ActionValue, ClosedBrace, MachineSpec, Some(_.concludeTransition())),
-    Transition(MachineSpec, Superstate, SuperstateValue, Some(_.addTransition())),
-    Transition(SuperstateValue, Name, SuperstateDeclaration, Some(_.markAsSuperstate())),
-    Transition(SuperstateDeclaration, OpenBrace, SubtransitionSpec, Some(_.markAsSubtransition())),
-    Transition(EventArrow, OpenBrace, SubtransitionSpec, Some(_.markAsSubtransition())),
-    Transition(SubtransitionSpec, Entry, EntryValue, None),
-    Transition(EntryValue, Name, SubtransitionSpec, Some(_.setEntryAction())),
-    Transition(SubtransitionSpec, Exit, ExitValue, None),
-    Transition(ExitValue, Name, SubtransitionSpec, Some(_.setExitAction())),
-    Transition(SubtransitionSpec, Event, SubeventValue, None),
-    Transition(SubeventValue, Name, SubNextStateArrow, Some(_.setEvent())),
-    Transition(SubNextStateArrow, Arrow, SubNextStateValue, None),
-    Transition(SubNextStateValue, Dash, SubactionArrow, Some(_.setEmptyNextState())),
-    Transition(SubNextStateValue, Name, SubactionArrow, Some(_.setNextState())),
-    Transition(SubactionArrow, ClosedBrace, MachineSpec, Some(_.concludeTransition())),
-    Transition(SubactionArrow, Entry, EntryValue, None),
-    Transition(SubactionArrow, Exit, ExitValue, None),
-    Transition(SubactionArrow, Arrow, SubactionDeclaration, None),
-    Transition(SubactionDeclaration, Name, SubtransitionSpec, Some(_.setActionAndConclude())),
-    Transition(SubactionDeclaration, OpenBrace, SubactionValue, None),
-    Transition(SubactionValue, Name, SubactionValue, Some(_.addAction())),
-    Transition(SubactionValue, ClosedBrace, SubtransitionSpec, None),
-    Transition(SubtransitionSpec, ClosedBrace, MachineSpec, Some(_.concludeTransition()))
-  )
+  private val transitions: List[Transition] =
+    machineSpecTransitions ++
+    transitionSpecTransitions ++
+    superstateTransitions ++
+    entryAndExitTransitions ++
+    subtransitionSpecTransitions
 
   private def handleEvent(event: SyntaxEvent, line: Int, position: Int): Unit =
     transitions.find(t => t.currentState == state && t.event == event) match {
--- src/main/scala/smc/syntaxAnalyzer/Transition.scala
+++
@@ -1,8 +0,0 @@
-package smc.syntaxAnalyzer
-
-final case class Transition(
-  currentState: SyntaxState,
-  event: SyntaxEvent,
-  newState: SyntaxState,
-  action: Option[SyntaxBuilder => Unit]
-)
