df66bb50082552e43236fd0839ff5e612439dde7
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-12-28 17:51:46 +0100

    Implement basic optimization

--- src/main/scala/smc/optimizer/Optimizer.scala
+++ src/main/scala/smc/optimizer/Optimizer.scala
@@ -6,6 +6,28 @@ final class Optimizer {
   def optimize(machine: StateMachine): OptimizedStateMachine = {
     val optimized = new OptimizedStateMachine(machine.name, machine.initialState)
 
+    addStates(machine, optimized)
+    addEvents(machine, optimized)
+    addActions(machine, optimized)
+
     optimized
   }
+
+  private def addStates(machine: StateMachine, optimized: OptimizedStateMachine): Unit =
+    machine.states
+      .filterNot(_.isSuperState)
+      .foreach(s => optimized.states += s.name)
+
+  private def addEvents(machine: StateMachine, optimized: OptimizedStateMachine): Unit =
+    for {
+      state <- machine.states
+      event <- state.events
+    } optimized.events += event.name
+
+  private def addActions(machine: StateMachine, optimized: OptimizedStateMachine): Unit =
+    for {
+      state <- machine.states
+      action <- state.entryActions ++ state.exitActions ++
+        state.events.flatMap(_.actions)
+    } optimized.actions += action
 }
--- src/test/scala/smc/OptimizationSuite.scala
+++ src/test/scala/smc/OptimizationSuite.scala
@@ -1,10 +1,9 @@
 package smc
 
 import munit.FunSuite
-
 import scala.collection.mutable.ListBuffer
 import smc.optimizer.{Optimizer, OptimizedStateMachine}
-import smc.syntaxAnalyzer.{StateMachineSyntax, StateMachine}
+import smc.syntaxAnalyzer.{StateMachineSyntax, StateMachine, State, Event}
 
 class OptimizationSuite extends FunSuite {
   protected var syntax = new StateMachineSyntax()
@@ -21,6 +20,100 @@ class BasicOptimizerSuite extends OptimizationSuite {
   test("Name of machine") {
     syntax.machines += new StateMachine("myMachine")
     val machines = optimize()
-    assertEquals("myMachine", "myMachine")
+    assertEquals(machines.head.name, "myMachine")
+  }
+
+  test("Name of initial state") {
+    syntax.machines += new StateMachine("") {
+      initialState = "initial"
+    }
+    val machines = optimize()
+    assertEquals(machines.head.initialState, "initial")
+  }
+
+  test("Multiple machines") {
+    syntax.machines += new StateMachine("hello") {
+      initialState = "world"
+    }
+    syntax.machines += new StateMachine("foo") {
+      initialState = "bar"
+    }
+
+    val machines = optimize()
+    assertEquals(machines.head.name, "hello")
+    assertEquals(machines.head.initialState, "world")
+    assertEquals(machines.last.name, "foo")
+    assertEquals(machines.last.initialState, "bar")
+  }
+
+  test("All states collected") {
+    syntax.machines += new StateMachine("") {
+      states += new State("state1")
+      states += new State("state2")
+      states += new State("state3")
+    }
+
+    val machines = optimize()
+    assertEquals(machines.head.states,
+      ListBuffer("state1", "state2", "state3"))
+  }
+
+  test("Superstates are ignored") {
+    syntax.machines += new StateMachine("") {
+      states += new State("state1")
+      states += new State("state2") {
+        isSuperState = true
+      }
+      states += new State("state3") {
+        isSuperState = true
+      }
+    }
+
+    val machines = optimize()
+    assertEquals(machines.head.states, ListBuffer("state1"))
+  }
+
+  test("All events collected") {
+    syntax.machines += new StateMachine("") {
+      states += new State("state1") {
+        events += new Event("event1")
+        events += new Event("event2")
+      }
+      states += new State("state2") {
+        events += new Event("event3")
+        events += new Event("event4")
+      }
+    }
+
+    val machines = optimize()
+    assertEquals(machines.head.events,
+      ListBuffer("event1", "event2", "event3", "event4"))
+  }
+
+  test("All actions collected") {
+    syntax.machines += new StateMachine("") {
+      states += new State("state1") {
+        events += new Event("event1") {
+          actions += "action1"
+          actions += "action2"
+        }
+        events += new Event("event2") {
+          actions += "action3"
+        }
+      }
+      states += new State("state2") {
+        events += new Event("event3") {
+          actions += "action4"
+        }
+        events += new Event("event4") {
+          actions += "action5"
+          actions += "action6"
+        }
+      }
+    }
+
+    val machines = optimize()
+    assertEquals(machines.head.actions,
+      ListBuffer("action1", "action2", "action3", "action4", "action5", "action6"))
   }
 }
