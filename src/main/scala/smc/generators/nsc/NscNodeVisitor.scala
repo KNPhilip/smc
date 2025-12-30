8ec05b7e2351ad920f12fbda1b8772f4067387ed
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-12-30 13:58:14 +0100

    Create node hierarchy for nested switch case

---
+++ src/main/scala/smc/generators/nsc/NscNodeVisitor.scala
@@ -0,0 +1,14 @@
+package smc.generators.nsc
+
+trait NscNodeVisitor {
+  def visit(node: SwitchCaseNode): Unit
+  def visit(node: CaseNode): Unit
+  def visit(node: DefaultCaseNode): Unit
+  def visit(node: FunctionCallNode): Unit
+  def visit(node: EnumNode): Unit
+  def visit(node: EnumeratorNode): Unit
+  def visit(node: StatePropertyNode): Unit
+  def visit(node: EventDelegatorsNode): Unit
+  def visit(node: HandleEventNode): Unit
+  def visit(node: FsmClassNode): Unit
+}
