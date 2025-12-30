ca2427a367f88c7ebd03bc5ce53440afc06f82c3
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-12-30 15:13:59 +0100

    Create NscNodeFactory

---
+++ src/main/scala/smc/generators/nsc/NscNodeFactory.scala
@@ -0,0 +1,59 @@
+package smc.generators.nsc
+
+import smc.optimizer.{OptimizedStateMachine, OptimizedTransition, OptimizedSubTransition}
+
+final class NscNodeFactory {
+  def generate(sm: OptimizedStateMachine): NscNode = {
+    val eventDelegators = EventDelegatorsNode(sm.events.toList)
+    val stateProperty = StatePropertyNode(sm.initialState)
+    val stateEnum = EnumNode("State", sm.states.toList)
+    val eventEnum = EnumNode("Event", sm.events.toList)
+    val stateSwitch = buildStateSwitch(sm)
+    val handleEvent = HandleEventNode(stateSwitch)
+
+    FsmClassNode(
+      className     = sm.name,
+      actionsName   = null,
+      delegators    = eventDelegators,
+      eventEnum     = eventEnum,
+      stateEnum     = stateEnum,
+      stateProperty= stateProperty,
+      handleEvent  = handleEvent,
+      actions      = sm.actions.toList,
+      states       = sm.states.toList
+    )
+  }
+
+  private def buildStateSwitch(sm: OptimizedStateMachine): SwitchCaseNode = {
+    val stateCases = sm.transitions.map(buildStateCase)
+    SwitchCaseNode(variableName = "state", caseNodes = stateCases.toList)
+  }
+
+  private def buildStateCase(t: OptimizedTransition): CaseNode = {
+    val eventSwitch = buildEventSwitch(t)
+    CaseNode(switchName = "State", caseName = t.currentState, caseActionNode = Some(eventSwitch))
+  }
+
+  private def buildEventSwitch(t: OptimizedTransition): SwitchCaseNode = {
+    val eventCases = t.subTransitions
+      .map(buildEventCase) :+ DefaultCaseNode(t.currentState)
+
+    SwitchCaseNode(variableName = "event", caseNodes = eventCases.toList)
+  }
+
+  private def buildEventCase(st: OptimizedSubTransition): CaseNode = {
+    val actions = buildActions(st)
+    CaseNode(switchName = "Event", caseName = st.event, caseActionNode = Some(actions))
+  }
+
+  private def buildActions(st: OptimizedSubTransition): CompositeNode = {
+    val setState = buildSetStateNode(st.nextState)
+    val actionCalls = st.actions.toList.map(FunctionCallNode(_))
+    CompositeNode(nodes = setState :: actionCalls)
+  }
+
+  private def buildSetStateNode(stateName: String): FunctionCallNode = {
+    val enumerator = EnumeratorNode("State", stateName)
+    FunctionCallNode(functionName = "setState", argument = Some(enumerator))
+  }
+}
