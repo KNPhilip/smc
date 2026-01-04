6a2be39b8ccbf41d8960ff5382f4fdd0b6c011c2
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2026-01-04 20:58:26 +0100

    Starting ArgumentParser

---
+++ src/main/scala/smc/ArgumentParser.scala
@@ -0,0 +1,12 @@
+package smc
+
+final class ArgumentParser(args: List[String]) {
+  var language: String = ""
+  var strategy: String = ""
+  var inputPath: String = ""
+  var outputPath: String = ""
+  
+  def run(): Unit = {
+
+  }
+}
